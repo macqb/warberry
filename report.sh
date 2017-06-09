58a8bf79c23b97544dd6b3bd3d312d4df17613df
DJ Hoxworth <macqb@users.noreply.github.com>
2017-06-09 12:59:39 -0700

    Create report.sh
    changed to nginx webserver
    made a change to the rm command to remove everything from Results dir, not just (dot) files

--- report.sh
+++ report.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#macQB - changed to nginx to be different
 #By itcarsales
 #Check for Contents in Server Results Folder
 #If reports are found, prompt to replace, then auto copy or skip
@@ -9,14 +10,15 @@ echo """
 if [ "$(ls -A /var/www/html/Results/)" ]; then
 	echo && read -p "Would you like to replace the existing report data? (y/n)" -n 1 -r -s reportReplace && echo
 	if [[ $reportReplace == "Y" || $reportReplace == "y" ]]; then
-		rm -rf /var/www/html/Results/*.*
+		#replaced rm -rf /var/www/html/Results/*.* cos remnants from previous scans remained
+		rm -rf /var/www/html/Results/*
 		cp -v /home/pi/WarBerry/Results/* /var/www/html/Results/
 	fi
 else
 	cp -v /home/pi/WarBerry/Results/* /var/www/html/Results/
 fi
-#Start Web Server
-service apache2 start
+#Start nginx Web Server
+service nginx start
 
 echo """
 	Reports are ready at:
@@ -25,6 +27,6 @@ echo """
     """
 
 echo && read -p "Press any key to stop the server and exit." -n 1 -r -s reportStop
-#Stop Web Server
-service apache2 stop
-echo && echo "Apache Service stopped: Exiting now......"
+#Stop nginx Web Server
+service nginx stop
+echo && echo "Nginx Web Server stopped: Exiting now......"
